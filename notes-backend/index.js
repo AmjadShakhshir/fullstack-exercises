0ac3efdf5f28cca01701177dac18b83cd5b622a6
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-20 23:08:55 +0200

    edit path for dist folder

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -1,13 +1,15 @@
 import express from "express";
-import morgan from "morgan";
 import cors from "cors";
+import path from "path";
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "dist")));
 
 let notes = [
   {
