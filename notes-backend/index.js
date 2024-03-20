d3b2c54064b7057f70531ab55e6e3bb83b49b1ec
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-20 23:03:55 +0200

    edit index.js

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static("dist"));
 
 let notes = [
   {
@@ -35,7 +36,6 @@ const requestLogger = (request, response, next) => {
 };
 
 app.use(requestLogger);
-app.use(express.static("dist"));
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
