d7bd8bbfb1ea0219f28381785e7dcc29a0ad3e0b
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-20 22:55:45 +0200

    change the dist file

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -35,6 +35,7 @@ const requestLogger = (request, response, next) => {
 };
 
 app.use(requestLogger);
+app.use(express.static("dist"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
