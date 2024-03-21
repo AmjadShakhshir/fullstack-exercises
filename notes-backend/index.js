22166b21a1c53978ae3f444470099740431bec38
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-22 00:30:42 +0200

    edit vercel.json

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -9,7 +9,7 @@ const __dirname = path.resolve();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "public")));
 
 let notes = [
   {
