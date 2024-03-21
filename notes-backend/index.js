34d10791a15cf3cfe2c76c3a8c084c70c5dcc431
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-22 00:34:39 +0200

    edit vercel.json

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -39,6 +39,10 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
