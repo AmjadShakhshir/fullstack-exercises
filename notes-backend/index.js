3bb26eac7b02d32e0ed5da524ecb5c1ae30ec0e5
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-03-22 00:38:07 +0200

    edit vercel.json

--- notes-backend/index.js
+++ notes-backend/index.js
@@ -43,6 +43,14 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to Api page</h1>");
+});
+
+app.get("/api", (req, res) => {
+  res.send("Hello from API");
+});
+
 app.get("/api/notes", (request, response) => {
   response.json(notes);
 });
