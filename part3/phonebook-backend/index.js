493cd4f1eea34a669cfe5fd81309fd23ca52c285
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-05-05 22:15:09 +0300

    add more error handlers to routes

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -53,7 +53,7 @@ app.get("/info", (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`);
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   PersonRepo.findById(request.params.id)
     .then((person) => {
       if (person) {
@@ -62,13 +62,10 @@ app.get("/api/persons/:id", (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: "malformatted id" });
-    });
+    .catch((error) => next(error));
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
   if (!body.name || !body.number) {
@@ -77,22 +74,69 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const person = new PersonRepo({
-    name: body.name,
-    number: body.number,
-  });
+  if (persons.find((person) => person.name === body.name)) {
+    const person = {
+      number: body.number,
+    };
 
-  person.save().then((savedPerson) => {
-    response.json(savedPerson);
-  });
+    PersonRepo.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then((updatedPerson) => {
+        response.json(updatedPerson);
+      })
+      .catch((error) => next(error));
+  } else {
+    const person = new PersonRepo({
+      name: body.name,
+      number: body.number,
+    });
+
+    person
+      .save()
+      .then((savedPerson) => {
+        response.json(savedPerson);
+      })
+      .catch((error) => next(error));
+  }
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const { name, number } = request.body;
+
+  PersonRepo.findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: "query" })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+  PersonRepo.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
